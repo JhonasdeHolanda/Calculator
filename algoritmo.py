3290972c3f70789db7cd1e0b324194e883f04da9
ceifer0 <sidney.andre.bda@gmail.com>
2025-06-08 16:46:50 -0300

    atualização 5

--- algoritmo.py
+++ algoritmo.py
@@ -1,3 +1,5 @@
+import math
+
 def tabuada(num1, num_final, operacao):
     if operacao == 1:
         return num1 + num_final
@@ -7,22 +9,39 @@ def tabuada(num1, num_final, operacao):
         return num1 * num_final
     elif operacao == 4:
         return num1 / num_final
+    elif operacao == 5:
+        return num1 ** num_final
 
 while True:
     print("Escolha a sua operação matemática")
-    print("1- soma\n2- subtração\n3- multiplicação\n4- divisão\n5- sair")
+    print("1- soma\n2- subtração\n3- multiplicação\n4- divisão\n5- potenciação\n6- raiz quadrada\n7- porcentagem\n8- sair")
     operacao = int(input("Digite sua operação escolhida: "))
     
-    if operacao == 5:
+    if operacao == 8:
         break
-    elif operacao not in [1, 2, 3, 4]:
-        print("Operação inválida! Tente novamente.")
+
+    elif operacao in [1, 2, 3, 4, 5]:
+        num1 = float(input("Digite o primeiro número: "))
+        num_final = float(input("Digite o segundo número: "))
+
+        resultado = tabuada(num1, num_final, operacao)
+        
+        operacoes = ["soma", "subtração", "multiplicação", "divisão", "potenciação"]
+        print(f"O resultado da {operacoes[operacao-1]} entre {num1} e {num_final} = {resultado}")
+
+    elif operacao == 6:
+        num = float(input("digite o número a ser calculado: "))
+        raiz = math.sqrt(num)
+        print(f"A raiz quadrada de {num} é: {raiz}")
+        
+    elif operacao == 7:
+        num = float(input("digite o primeiro número: "))
+        porcentagem = float(input("digite a porcentagem desejada: "))
+        conta = num * (porcentagem / 100)
+        print(f"os {porcentagem}% de {num} é: {conta}")
+
+    else:
+        print("\n\nOperação inválida! Tente novamente.\n \n")
         continue
     
-    num1 = float(input("Digite o primeiro número: "))
-    num_final = float(input("Digite o segundo número: "))
-    
-    resultado = tabuada(num1, num_final, operacao)
-    
-    operacoes = ["soma", "subtração", "multiplicação", "divisão"]
-    print(f"O resultado da {operacoes[operacao-1]} entre {num1} e {num_final} = {resultado}")
+   
